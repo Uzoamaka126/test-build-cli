473705f143e16368be494d1dfb390abf2fcb3916
Uzoamaka126 <uzoamakaanyanwu26@gmail.com>
2023-08-21 18:19:04 +0100

    add: more actions and a flag

--- index.ts
+++ index.ts
@@ -1,7 +1,53 @@
 #!/usr/bin/env node
 
-function hello(message: string) {
-    return `${message} is a ${typeof message}`
-}
+import { Command } from 'commander';
 
-console.log('Hello worls');
+// Declare the program
+const program = new Command();
+
+// Add actions onto the CLI
+// program
+//     .name("string-util")
+//     .argument("<string>", "string to log")
+//     .description('CLI to some JS string utilities')
+//     .version('0.8.0')
+//     .action((message: string) => {
+//     console.log(`Hello ${message}!!!`);  
+// });
+
+program
+    .argument("<string>", "string to log")
+    .option("-c, --capitalize", "Capitalize the message")
+    .description('CLI to some JS string utilities')
+    // .version('0.0.1')
+    .action((
+        message: string, 
+        options: { capitalize?: boolean } 
+    ) => {
+        if (options.capitalize) {
+            console.log(message.toUpperCase());
+        } else {
+            console.log(message);
+        }
+    });
+
+program
+    .command("add <numbers>")
+    .action((numbers: number[]) => {
+        const total = numbers.reduce((a, b) => a + b, 0);
+        console.log(`Total: ${total}`);
+    })
+    .description("Add numbers and log the total")
+
+
+program
+    .command("get-max-number <numbers>")
+    .action((numbers: number[]) => {
+        const max = Math.max(...numbers)
+        console.log(`Max: ${max}`);
+    })
+    .description("Get a max number from an array of numbers")
+
+// Execute the CLI with the given arguments
+program.parse(process.argv);
+
